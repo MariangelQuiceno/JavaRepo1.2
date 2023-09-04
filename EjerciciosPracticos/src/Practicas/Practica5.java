a65a3cfc80b78e3fe008d70ffd9774bf43c261eb
MariangelQuiceno <126704306+MariangelQuiceno@users.noreply.github.com>
2023-09-04 12:32:49 -0500

    JavaJava

--- EjerciciosPracticos/src/Practicas/Practica5.java
+++ EjerciciosPracticos/src/Practicas/Practica5.java
@@ -1,5 +1,30 @@
 package Practicas;
 
 public class Practica5 {
+  
+   	 public static void main(String[] args) {
+	        int[] arreglo1 = {1, 2, 3};
+	        int[] arreglo2 = {3,2,1};
+
+	        int[] resultado = sumaArreglos(arreglo1, arreglo2);
+	        System.out.print("Resultado de la suma: ");
+	        for (int i = 0; i < resultado.length; i++) {
+	            System.out.print(resultado[i] + " ");
+	        }
+	    }
+	
+	    public static int[] sumaArreglos(int[] arreglo1, int[] arreglo2) {
+	        int[] resultado = new int[arreglo1.length]; 
+
+	        for (int i = 0; i < arreglo1.length; i++) {
+	            resultado[i] = arreglo1[i] + arreglo2[i]; 
+	        }
+
+	        return resultado; 
+	    }
+
+	   
+	}
+
 
 }
